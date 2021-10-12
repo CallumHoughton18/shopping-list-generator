eb070f0b65a6010fb9381ffcc011c8b548af570f
Callum Houghton <callum.houghton13@hotmail.co.uk>
2021-10-12 12:12:14 +0100

    Add integration tests. Handle empty new lines (#1)
    * add injection of arguments to main function for testability
    
    * add integration tests to test_shopping_list_generator.py with mocked file system
    
    * add test for invalid recipe format
    
    * discard configured_mock_open object in test_shopping_list_generator tests

--- src/shopping_list_generator/__main__.py
+++ src/shopping_list_generator/__main__.py
@@ -1,14 +1,16 @@
 import argparse
 import os
+import sys
+from typing import List
 
-from shopping_list_generator.recipelocator import read_and_parse_recipe
+from shopping_list_generator.recipelocator import RecipeParsingError, read_and_parse_recipe
 from shopping_list_generator.shoppinglist import ShoppingList
 
 
-def main() -> None:
+def main(argv: List[str]) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("recipes", help="relative path of the recipe files")
-    args = parser.parse_args()
+    args = parser.parse_args(argv[1:])
 
     # Strip white space and append .txt to filename string if it does not end with .txt
     recipe_file_names = [x.strip() + '.txt' if not x.endswith('.txt') else x.strip()
@@ -20,6 +22,10 @@ def main() -> None:
             shopping_list.add_recipe(recipe)
         except IOError as err:
             print(f"Could not read the recipe file {recipe_file_name}: {err}")
+            return 1
+        except RecipeParsingError as err:
+            print(f"Could not parse recipe {recipe_file_name}: {err}")
+            return 1
 
     shopping_list_text = shopping_list.get_shopping_list_text()
 
@@ -31,7 +37,10 @@ def main() -> None:
             filetowrite.write(shopping_list_text)
     except IOError as err:
         print(f"Could not write to file {shopping_list_path}: {err}")
+        return 1
+
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv))
---
+++ src/tests/test_shopping_list_generator.py
@@ -0,0 +1,60 @@
+
+from typing import List
+from unittest.mock import mock_open, patch
+import pytest
+import shopping_list_generator.__main__
+
+
+def configure_mock_open(mo, mock_file_contents: List[str]):
+    write_mock = mock_open(read_data="")
+    handlers = (mo.return_value,
+                *[mock_open(read_data=content).return_value for content in mock_file_contents],
+                write_mock.return_value)
+    mo.side_effect = handlers
+    return mo, write_mock
+
+
+class TestIntegrationCLITests:
+    def test_should_fail_if_no_recipes_given(self):
+        with pytest.raises(SystemExit) as wrapped_exception:
+            shopping_list_generator.__main__.main(['main.py'])
+        assert wrapped_exception.type == SystemExit
+        assert wrapped_exception.value.code == 2
+
+    def test_should_fail_if_recipe_does_not_exist(self):
+        return_code = shopping_list_generator.__main__.main(['main.py', 'test_recipes/daassl'])
+        # Should fail as recipe does not exist
+        assert return_code == 1
+
+    @patch("builtins.open", new_callable=mock_open, read_data="1 onion\n1 pepper\n1 lentil")
+    def test_should_successfully_generate_shopping_list(self, mo):
+        _, write_mock = configure_mock_open(mo, ['1 onion\n1 pepper\n1 spaghetti'])
+
+        shopping_list_generator.__main__.main(['main.py', 'daal, spaghetti'])
+
+        write_mock().write.assert_called_once_with("2 onion\n2 pepper\n1 lentil\n1 spaghetti")
+
+    @patch("builtins.open", new_callable=mock_open, read_data="1 onion\n1 pepper\n1 lentil")
+    def test_should_successfully_generate_shopping_list_and_handle_case(self, mo):
+        _, write_mock = configure_mock_open(mo, ['1 Onion\n1 Pepper\n1 spaghetti'])
+
+        shopping_list_generator.__main__.main(['main.py', 'daal, spaghetti'])
+
+        write_mock().write.assert_called_once_with("2 onion\n2 pepper\n1 lentil\n1 spaghetti")
+
+    @patch("builtins.open", new_callable=mock_open, read_data="1 onion\n1 pepper\n1 lentil")
+    def test_should_successfully_handle_empty_lines(self, mo):
+        _, write_mock = configure_mock_open(mo, ['1 onion\n\n1 pepper\n1 spaghetti\n\n\n'])
+
+        shopping_list_generator.__main__.main(['main.py', 'daal, spaghetti'])
+
+        write_mock().write.assert_called_once_with("2 onion\n2 pepper\n1 lentil\n1 spaghetti")
+
+    @patch("builtins.open", new_callable=mock_open, read_data="1 onion\n1 pepper\n1 lentil")
+    def test_should_fail_on_invalid_recipe(self, mo):
+        _, write_mock = configure_mock_open(mo, ['1:onion\n\n1: pepper\n1 spaghetti\n\n\n'])
+
+        exit_code = shopping_list_generator.__main__.main(['main.py', 'daal, spaghetti'])
+        assert exit_code == 1
+
+        write_mock().write.never_called()
