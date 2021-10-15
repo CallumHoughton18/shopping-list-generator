a8a4ff258cdae4cedbb3e1686a585f12e13bb413
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-10-15 11:56:16 +0100

    change script entrypoint in setup.py

--- src/setup.py
+++ src/setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "shopping-list-generator=shopping_list_generator.__main__:main",
+            "shopping-list-generator=shopping_list_generator.__main__",
         ]
     },
 )
